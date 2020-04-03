4c024aa7380df643c97d1f71c2841f0c0250b0cf
melankolia <setyo.nugroho96@gmail.com>
2020-04-04 00:24:25 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld message="Welcome to Your Vue.js App"/>
+    <HelloWorld message="Pokemon JS"/>
   </div>
 </template>
 
