772c439a4bf8194cf587f1db366e6303ff9f4ad4
ahmad ridwan <ahmadridwan.3107@gmail.com>
2020-07-06 23:25:43 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules omitted
+
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          hotReload: true // disables Hot Reload
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  // plugin omitted
+}
